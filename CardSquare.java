54d28ccfac428852f3d648478d0564f94f6ffbd9
Muhammad K Khan <mk.muhammad.khan10@gmail.com>
2015-10-13 20:30:41 -0500

    remove old files
    removed old files

--- CardSquare.java
+++
@@ -1,6 +0,0 @@
-/**
- * Created by muhammadkhalilullah on 10/11/2015.
- */
-public class CardSquare
-{
-}
--- CornerSquare.java
+++
@@ -1,9 +0,0 @@
-public class CornerSquare extends BoardLocation
-{
-
-	@Override
-	public String[] getPossibleActions(Player player) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-}
