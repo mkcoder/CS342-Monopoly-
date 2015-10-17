9152b3b604ceaa2eac0d066314f6c61a56f0270f
mkhk352 <mkhk352@gmail.com>
2015-10-17 17:04:20 -0500

    coordArray

--- src/Applet.java
+++ src/Applet.java
@@ -12,8 +12,11 @@ public class Applet extends JApplet
 	public static final String BOARD_FILE = "./src/mon.jpg";
 	public static final int OFFSET_X = 10;
 	public static final int OFFSET_Y = 10;
+	private Point  [] coordArray;
 	
 	BufferedImage boardImage;
+	
+	
 	
 	public void init()
 	{
@@ -23,16 +26,57 @@ public class Applet extends JApplet
 	@Override
 	public void paint(Graphics g)
 	{
+	    
 		int imgScale;
 		
 		super.paint(g);
 		
-		if(getWidth() < getHeight())
-			imgScale = getWidth() - 20;
+		 
+		
+		if((getWidth() - 130) < getHeight())
+			imgScale = getWidth() - 150;
 		else
 			imgScale = getHeight() - 20;
 		
+		coordArray = getPoints(imgScale);
+		
 		g.drawImage(boardImage, OFFSET_X, OFFSET_Y, imgScale, imgScale, null);
+		
+		
+	}
+	
+	private Point[] getPoints(int imageScale)
+	{
+	    Point [] tempCoord;
+	    int divFactor;
+	 
+	    
+	    tempCoord = new Point[40];
+	    divFactor = imageScale/9;
+	    
+	    for(int i = 0; i < 11 ;i++)
+	    {
+	        tempCoord[i] = new Point(imageScale-divFactor*i,imageScale);
+	    }
+	    
+	    for (int i = 11; i < 21; i++)
+	    {
+	        tempCoord[i] = new Point(0,imageScale-divFactor*(i-11));
+        }
+	    
+	    for (int i = 21; i < 31; i++)
+        {
+            tempCoord[i] = new Point(divFactor*(i-21),0);
+        }
+	    
+	    for (int i = 31; i < 40; i++)
+        {
+            tempCoord[i] = new Point(imageScale,divFactor*(i-31));
+        }
+	    
+	    
+	    
+        return tempCoord;
 	}
 	
 	public BufferedImage getImage(String fileName)
