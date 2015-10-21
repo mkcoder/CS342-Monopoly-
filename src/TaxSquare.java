bee48e5a50bed30518df9738cc8d8251a835929a
Muhammad K Khan <mk.muhammad.khan10@gmail.com>
2015-10-20 22:32:26 -0500

    comment
    added comments to classes

--- src/TaxSquare.java
+++ src/TaxSquare.java
@@ -3,15 +3,19 @@ package src;
 
 public class TaxSquare extends BoardLocation
 {
-	private final int tax;
+	private final int tax;			// the amount of tax to be collected from the player ($)
 	
 	public TaxSquare(String name, int address, int tax)
+	// PRE: name, address, and tax is initialized tax >= 0, 0 <= address <= 15
+	// POST: Create a new TaxSquare object with name set to name, address set to address and tax set to tax
 	{
 		super(name, address);
 		this.tax = tax;
 	}
 
 	public int payTax(Player player)
+    // PRE: player is initialized and is of type player
+    // POST: subtracts the tax amount from the player and returns the tax amount collected
 	{	    
 	    player.addMoney(-1*tax);
 	    return tax;
@@ -19,6 +23,9 @@ public class TaxSquare extends BoardLocation
 	
 	@Override
 	public String[] getPossibleActions(Player player)
+    // PRE: player is initialized and is of type player
+    // POST: return a string list of the possible actions player can do next
+    // fctnval == array of possible next move
 	{
 		// TODO Auto-generated method stub
 		return null;
--- src/Utility.java
+++ src/Utility.java
@@ -3,23 +3,29 @@ package src;
 
 public class Utility extends Property
 {
-	private Utility other;
+	private Utility other;			// the other utility the person can also purchase
 	
 	public Utility(String name, int address, int cost)
+	// PRE: name, address, and cost is initialized. 0 <= address <= 39, cost >= 0
+    // POST: create a new utility object that has name set to name, address set to address and cost set to cost
 	{
 		super(name, address, cost);
 	}
 
 	@Override
 	public void collectRent(Player player)
+    // PRE:  player is initialized and is of type player
+    // POST: subtract amount of rent from the player current earnings ($)
 	{
-		int rent;		
+		int rent;       // the amount of rent to collect from player
 		
 		rent = other.owner == this.owner ? Player.getDice() * 10 : Player.getDice() * 4;
 		player.transferMoneyTo(owner, rent);
 	}
 	
 	public static void setOther(Utility water, Utility electric)
+    // PRE: wave and electric is initialized
+    // POST: does a basic swap by setting each other to each other
 	{
 		water.other = electric;
 		electric.other = water;
@@ -27,12 +33,17 @@ public class Utility extends Property
 
 	@Override
 	public String[] getPossibleActions(Player player)
+    // PRE: player is initialized and is of type player
+    // POST: return a string list of the possible actions player can do next
+    // fctnval == array of possible next move
 	{
-		return null;
+        return null;
 	}
 	
 	@Override
 	public String toString()
+    // POST: returns a string object
+    // fctnval == return a formatted string with location + " COST $" + cost + " Owned by " + player " + ".";
 	{
 		return super.toString();
 	}
