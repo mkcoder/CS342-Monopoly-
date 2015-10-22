55f394b79a3ff2f32da62c62086e9825085edb1c
MaciejSzpakowski <systemzajety@yahoo.com>
2015-10-21 19:42:13 -0500

    minor changes

--- src/BoardLocation.java
+++ src/BoardLocation.java
@@ -3,9 +3,9 @@ package src;
 
 public abstract class BoardLocation
 {
-    protected final String name;								// piece name     
-    protected final int address;								// the distance from go
-    protected BoardLocation next;						// the next place to move
+    protected final String name;   // piece name     
+    protected final int address;   // the distance from go
+    protected BoardLocation next;  // the next place to move
 
 	public BoardLocation(String name, int address)
 	// PRE: name and address are intialized,
@@ -20,33 +20,33 @@ public abstract class BoardLocation
 	public abstract String[] getPossibleActions(Player player);
 
     public int getAddress() 
-    // FCTVAL = an integer value the denotes the dinstace from go
+    // POST:FCTVAL = an integer value the denotes the dinstace from go
     {
         return address;
     }
 
     public BoardLocation getNext()
-    // FCTVAL = the next sequential location after this
+    // POST:FCTVAL = the next sequential location after this
     {
         return next;
     }
     
     public String getName()
-    // FCTVAL = A string that is this location's name         
+    // POST:FCTVAL = A string that is this location's name         
     {
     	return name;
     }
     
     @Override
     public String toString()
-    // FCTVAL = A string the describes properties name, address, cost.         
+    // POST:FCTVAL = A string the describes properties name, address, cost.         
     {
     	return "Location: " + name + ".";
     }
     
     public static void Link(BoardLocation[] board)
     // PRE: board is an array of BoardLocations with all objects initialized
-    // POST: All the boardLocation objects will be linked starting for index 0 to index n -1
+    // POST: All the boardLocation objects will be linked in circular manner
     {
     	for(int i=0;i<board.length - 1;i++)
     		board[i].next = board[i+1];
